05899fe84376df381621a55f7028f4c3a2ce1c15
Zenky <bzenky@gmail.com>
2022-07-12 21:37:12 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header"
+
 function App() {
 
   return (
-    <h1>Znk</h1>
+    <Header />
   )
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.scss'
+import todoLogoImg from '../../assets/logo-todo.svg'
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={todoLogoImg} alt="Logo Todo" width="126" height="48" />
+        </header>
+    )
+}
