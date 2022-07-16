a1da422ea1d5718a97cc0b82f1ee9843e0183071
Zenky <bzenky@gmail.com>
2022-07-16 14:40:22 -0300

    structure

--- src/components/Task/TaskAdd/index.tsx
+++ src/components/Task/TaskAdd/index.tsx
@@ -1,15 +1,38 @@
+import { FormEvent } from 'react'
 import styles from './TaskAdd.module.scss'
 
 import addIcon from '../../../assets/icon-add.svg'
 
-export function TaskAdd() {
+interface Task {
+    id: string
+    content: string
+}
+interface TaskAddProps {
+    handleAddTask: (event: FormEvent) => void
+    onInputChange: (content: string) => void
+    newTask: Task
+}
+
+export function TaskAdd({ handleAddTask, onInputChange, newTask }: TaskAddProps) {
     return (
         <div className={styles.taskAddWrapper}>
-            <input type="text" placeholder="Adicione uma nova tarefa" className={styles.taskInput} />
-            <button className={styles.taskCreateButton} title="Botão para adicionar uma nova tarefa">
-                Criar
-                <img src={addIcon} alt="Ícone de adicionar" />
-            </button>
+            <form className={styles.newTaskForm}>
+                <input
+                    type="text"
+                    placeholder="Adicione uma nova tarefa"
+                    className={styles.taskInput}
+                    onChange={(event) => onInputChange(event.target.value)}
+                    value={newTask.content}
+                />
+                <button
+                    className={styles.taskCreateButton}
+                    title="Botão para adicionar uma nova tarefa"
+                    onClick={handleAddTask}
+                >
+                    Criar
+                    <img src={addIcon} alt="Ícone de adicionar" />
+                </button>
+            </form>
         </div>
     )
 }
--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,14 +1,58 @@
-import styles from './Task.module.scss';
+import { FormEvent, useState } from 'react'
+import styles from './Task.module.scss'
 
-import { TaskAdd } from "./TaskAdd";
-import { TaskList } from "./TaskList";
+import { TaskAdd } from "./TaskAdd"
+import { TaskList } from "./TaskList"
+
+interface Task {
+    id: string
+    content: string
+    isTaskDone: boolean
+}
 
 export function Task() {
+    const [taskList, setTaskList] = useState<Task[]>([]);
+    const [newTask, setNewTask] = useState<Task>({ content: '', id: '', isTaskDone: false });
+    const id = String(new Date().getTime())
+
+    function handleAddTask(event: FormEvent) {
+        event.preventDefault()
+        const verifyContentExists = taskList.filter(task => task.content === newTask.content)
+
+        if (newTask.content.trim() !== '' && verifyContentExists.length === 0) {
+            setTaskList([...taskList, newTask])
+        }
+        setNewTask({ content: '', id: '', isTaskDone: false })
+    }
+
+    function inputEventValue(value: string) {
+        setNewTask({ content: value, id: id, isTaskDone: false })
+    }
+
+    function handleTaskDone(id: string) {
+        const taskListDoneUpdated = taskList.map(task => task.id === id ? { ...task, isTaskDone: !task.isTaskDone } : task)
+        setTaskList(taskListDoneUpdated)
+    }
+
+    function handleRemoveTask(id: string) {
+        const taskListUpdated = taskList.filter(task => task.id !== id)
+
+        setTaskList(taskListUpdated)
+    }
+
     return (
         <main className={styles.main}>
-            <TaskAdd />
+            <TaskAdd
+                onInputChange={inputEventValue}
+                handleAddTask={handleAddTask}
+                newTask={newTask}
+            />
 
-            <TaskList />
+            <TaskList
+                taskList={taskList}
+                handleTaskDone={handleTaskDone}
+                handleRemoveTask={handleRemoveTask}
+            />
         </main>
     )
 }
