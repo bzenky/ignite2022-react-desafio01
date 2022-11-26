f727ce9035890b60ccee6938ade9949b94d19e94
Zenky <bzenky@gmail.com>
2022-11-26 18:42:19 -0300

    feat: drag and drop

--- src/components/Task/TaskList/index.tsx
+++ src/components/Task/TaskList/index.tsx
@@ -6,13 +6,39 @@ import { TaskItem } from '../TaskItem'
 import { EditModal } from '../../EditModal'
 
 import { TaskListContainer, TaskListHeader } from './styles'
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd'
 
 export function TaskList() {
     const [open, setOpen] = useState(false)
-    const { taskList } = useContext(ToDoContext)
+    const { taskList, handleUpdateTaskList } = useContext(ToDoContext)
 
     function handleModal() {
         setOpen(!open)
+    }
+
+    const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
+        // Estilização do Item quando estiver realizando o drag
+        outline: isDragging && '2px solid #8284FA',
+        outlineOffset: '4px',
+        borderRadius: '8px',
+        ...draggableStyle,
+    })
+
+    const getListStyle = (isDraggingOver: boolean) => ({
+        // Estilização da Lista quando estiver realizando o drag
+        background: isDraggingOver ? '#22222280' : '',
+        transition: 'all 0.2s ease-out',
+        borderRadius: '8px',
+    })
+
+    function handleOnDragEnd(result: DropResult) {
+        if (!result.destination) return
+
+        const items = Array.from(taskList)
+        const [reorderedItem] = items.splice(result.source.index, 1)
+        items.splice(result.destination.index, 0, reorderedItem)
+
+        handleUpdateTaskList(items)
     }
 
     return (
@@ -32,17 +58,48 @@ export function TaskList() {
             {taskList.length === 0
                 ? <EmptyTaskList />
                 : (
-                    <Dialog.Root open={open} onOpenChange={setOpen}>
-                        {taskList.map(task => {
-                            return (
-                                <TaskItem
-                                    key={task.id}
-                                    task={task}
-                                />
-                            )
-                        })}
-                        <EditModal handleModal={handleModal} />
-                    </Dialog.Root>
+                    <DragDropContext onDragEnd={handleOnDragEnd}>
+                        <Droppable droppableId="droppable-area" direction="vertical">
+                            {(provided, snapshot) => (
+                                <div
+                                    {...provided.droppableProps}
+                                    ref={provided.innerRef}
+                                    style={getListStyle(snapshot.isDraggingOver)}
+                                >
+                                    {taskList.map((task, index) => {
+                                        return (
+                                            <Draggable key={task.id} draggableId={task.id} index={index}>
+                                                {(provided, snapshot) => (
+                                                    <Dialog.Root
+                                                        open={open}
+                                                        onOpenChange={setOpen}
+                                                    >
+                                                        <div
+                                                            ref={provided.innerRef}
+                                                            {...provided.draggableProps}
+                                                            {...provided.dragHandleProps}
+                                                            key={task.id}
+                                                            style={getItemStyle(
+                                                                snapshot.isDragging,
+                                                                provided.draggableProps.style
+                                                            )}
+                                                        >
+                                                            <TaskItem
+                                                                task={task}
+                                                            />
+                                                        </div>
+
+                                                        <EditModal handleModal={handleModal} />
+                                                    </Dialog.Root>
+                                                )}
+                                            </Draggable>
+                                        )
+                                    })}
+                                    {provided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 )
             }
         </TaskListContainer>
