f2d476faafb4a487240390b8ea807e08282f7f26
Zenky <bzenky@gmail.com>
2022-12-26 18:10:57 -0300

    Release 3.1.2  (#4)
    * feat: ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import ReactGA from 'react-ga'
 import { ToDoContextProvider } from "./contexts/ToDoContext"
 import { Header } from "./components/Header"
 import { Task } from "./components/Task"
 import { GlobalStyle } from "./styles/global"
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default"
+import { useEffect } from 'react'
+import { SetGA } from './lib/gtag'
 
 function App() {
+  useEffect(() => {
+    SetGA()
+  }, [])
+
   return (
     <ToDoContextProvider>
       <ThemeProvider theme={defaultTheme}>
