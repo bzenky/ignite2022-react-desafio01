2640abe76ea1d88fec605ad643911192caae2d14
bzenky <bzenky@gmail.com>
2024-08-23 20:39:06 -0300

    chore: details modal text

--- src/components/Modal/Details/index.tsx
+++ src/components/Modal/Details/index.tsx
@@ -1,40 +1,28 @@
-import { DialogClose } from "@radix-ui/react-dialog"
-import { TaskProps } from "../../Task/TaskItem"
-import { ButtonWrapper, DetailsWrapper } from "./styles"
-import { GetLocalStorageItem } from "../../../utils/localStorage"
+import { DialogClose } from "@radix-ui/react-dialog";
+import { TaskProps } from "../../Task/TaskItem";
+import { ButtonWrapper, DetailsWrapper } from "./styles";
+import { GetLocalStorageItem } from "../../../utils/localStorage";
 
 export function Details() {
-  const currentTask: TaskProps = GetLocalStorageItem('currentTask') || '{}'
-  const { createdAt, doneAt, editedAt } = currentTask
+  const currentTask: TaskProps = GetLocalStorageItem("currentTask") || "{}";
+  const { createdAt, doneAt, editedAt } = currentTask;
 
   return (
     <>
-      <h2 data-cy="detailsModalTitle">
-        Detalhes da tarefa
-      </h2>
+      <h2 data-cy="detailsModalTitle">Detalhes da tarefa</h2>
 
       <DetailsWrapper>
-        <span>
-          Tarefa criada em {createdAt}
-        </span>
+        <span>Criada em {createdAt}</span>
 
-        {editedAt &&
-          <span>
-            Última edição em {editedAt}
-          </span>
-        }
+        {editedAt && <span>Última edição em {editedAt}</span>}
 
-        {doneAt &&
-          <span>
-            Tarefa concluída em {doneAt}
-          </span>
-        }
+        {doneAt && <span>Concluída em {doneAt}</span>}
 
         <ButtonWrapper>
           <DialogClose asChild>
             <button
               type="button"
-              className='cancel'
+              className="cancel"
               data-cy="detailsBackButton"
             >
               Voltar
@@ -43,5 +31,5 @@ export function Details() {
         </ButtonWrapper>
       </DetailsWrapper>
     </>
-  )
+  );
 }
